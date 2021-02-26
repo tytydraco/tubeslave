e3727886b1b3e11a6a2dba66389e5c704b0ca5b3
Tyler Nijmeh <tylernij@gmail.com>
2021-02-25 20:38:05 -0800

    DONE
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- post.sh
+++ post.sh
@@ -6,5 +6,9 @@ NEWPATH="$ORIGINAL_PATH"
 # Replace || with |
 NEWPATH="$(echo "$NEWPATH" | sed 's/||/|/g')"
 
+echo ""
+echo -e "DONE: \033[0;31m$NEWPATH\033[0m"
+echo ""
+
 [[ "$ORIGINAL_PATH" != "$NEWPATH" ]] && mv "$ORIGINAL_PATH" "$NEWPATH"
 exit 0
