039456e70b38b94dd090cc3ffcc7159b1fde4579
Tyler Nijmeh <tylernij@gmail.com>
2021-02-26 13:37:26 -0800

    Pull from web
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- tubeslave.sh
+++ tubeslave.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-LIST="list.txt"
+LIST_URL="https://gist.githubusercontent.com/tytydraco/b046cd249b6a75d5806398e9872746a1/raw/0f0f187ad08b93b8dcb022e359362e08ef88e9f8/list.txt"
 ARCHIVES="archives"
 DOWNLOADS="downloads"
 FORMAT="%(uploader)s|%(creator)s|%(title)s.%(ext)s"
 POST_SCRIPT="post.sh"
-FORMAT="mp3"
+AUDIO_FORMAT="mp3"
 
 mkdir -p "$ARCHIVES"
 
@@ -22,7 +22,7 @@ download() {
         -i \
         -x \
         --download-archive "$ARCHIVES/$name.txt" \
-        --audio-format "$FORMAT" \
+        --audio-format "$AUDIO_FORMAT" \
         --audio-quality 0 \
         --embed-thumbnail \
         --add-metadata \
@@ -33,10 +33,11 @@ download() {
         "$url"
 }
 
+list_content="$(curl -s "$LIST_URL")"
 while IFS= read -r line
 do
     name="$(echo "$line" | awk -F'|' '{print $1}' | xargs)"
     url="$(echo "$line" | awk -F'|' '{print $2}' | xargs)"
 
     download "$name" "$url" &
-done < "$LIST"
+done <<< "$list_content"
