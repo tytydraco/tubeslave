cfe89ed5788108eed3efb0cdd7bf36dead4e6fce
Tyler Nijmeh <tylernij@gmail.com>
2021-03-02 23:57:16 -0800

    Fix
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- post.sh
+++ post.sh
@@ -4,7 +4,7 @@ ORIGINAL_PATH="$*"
 NEWPATH="$ORIGINAL_PATH"
 
 # Some songs have a title including the artist name. Remove the author. 
-NEWPATH="$(echo "$NEWPATH" | sed 's/.*?- //')"
+NEWPATH="$(echo "$NEWPATH" | sed 's/.*- //')"
 
 echo ""
 echo -e "\033[0;31mDOWNLOADED: $NEWPATH\033[0m"
